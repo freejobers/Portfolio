03ed6bc04409782d87edf4c5c26bc35f9943bd73
freejobers <freejobers@gmail.com>
2019-09-10 01:18:10 +0300

    seting project

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var less = require('gulp-less');
-var path = require('path');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
  
 gulp.task('less', function () {
   return gulp.src('./css/less/**/*.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('./css'));
+  	.pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(concat('css/style.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('sass', function () {
@@ -18,8 +20,8 @@ gulp.task('sass', function () {
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./css/sass/**/*.scss', gulp.parallel('sass'));
+  gulp.watch('./css/sass/**/*.scss', gulp.series('sass'));
 });
 gulp.task('less:watch', function () {
-  gulp.watch('./css/less/**/*.less', gulp.parallel('less'));
+  gulp.watch('./css/less/**/*.less', gulp.series('less'));
 });
