51ef30af6071d1a41682f6e9a3b2d208b68f4211
freejobers <freejobers@gmail.com>
2019-09-10 15:55:06 +0300

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,33 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var less = require('gulp-less');
+var gutil = require('gulp-util');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require("browser-sync").create();
  
 gulp.task('less', function () {
   return gulp.src('./css/less/**/*.less')
   	.pipe(sourcemaps.init())
-    .pipe(less())
+    .pipe(less().on('error', function(err){
+        gutil.log(err);
+        this.emit('end');
+    }))
     .pipe(concat('css/style.css'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task('sass', function () {
-  return gulp.src('./css/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
+gulp.task('serve', gulp.series('less',  function(done) {
+
+  browserSync.init({
+    server: "./"  
+  });
+
+  gulp.watch(['css/less/**/*.less'], gulp.series('less'));
+  gulp.watch("*.html").on('change', browserSync.reload);
+  done();
+}));
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./css/sass/**/*.scss', gulp.series('sass'));
-});
-gulp.task('less:watch', function () {
-  gulp.watch('./css/less/**/*.less', gulp.series('less'));
-});
