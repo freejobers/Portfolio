9e66cf3af5c0a147f954c39ec07557eb6f605410
freejobers <freejobers@gmail.com>
2019-09-18 21:44:38 +0300

    add jquery filter for works portfolio

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,21 @@ const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require("browser-sync").create();
 const notify = require('gulp-notify');
 const autoprefixer = require('gulp-autoprefixer');
- 
+const csscomb = require('gulp-csscomb');
+
 gulp.task('less', function () {
   return gulp.src('./css/less/style.less')
-  	.pipe(sourcemaps.init())
-    .pipe(less().on('error', notify.onError(function(err) {
-    	return {
-    		title: 'less',
-    		message: err.message
-    	};
+    .pipe(sourcemaps.init())
+    .pipe(less().on('error', notify.onError(function (err) {
+      return {
+        title: 'less',
+        message: err.message
+      };
     })))
     .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
+      browsers: ['last 1 versions']
+    }))
+    .pipe(csscomb())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./css/'))
     .pipe(browserSync.reload({
@@ -26,14 +28,14 @@ gulp.task('less', function () {
     }));
 });
 
-gulp.task('serve', gulp.series('less',  function(done) {
+gulp.task('serve', gulp.series('less', function (done) {
 
   browserSync.init({
-    server: "./"  
+    server: "./"
   });
 
   gulp.watch(['css/less/**/*.less'], gulp.series('less'));
   gulp.watch("*.html").on('change', browserSync.reload);
   done();
 }));
- 
+
